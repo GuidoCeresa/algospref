611786cdb2ffd9a4a7965d6030eb4bf335cb8d64
GuidoCeresa <gac@algos.it>
2014-06-29 09:39:30 +0200

    0.70

--- AlgosprefGrailsPlugin.groovy
+++ AlgosprefGrailsPlugin.groovy
@@ -3,7 +3,7 @@
 //
 class AlgosprefGrailsPlugin {
     // the plugin version
-    def version = "0.69"
+    def version = "0.70"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4.2 > *"
     // the other plugins this plugin depends on
