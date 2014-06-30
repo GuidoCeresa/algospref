3e8a43665b5de717ff0028ce4e0b714079d85e40
GuidoCeresa <gac@algos.it>
2014-06-30 11:51:11 +0200

    copyFile and moveFile

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -14,26 +14,37 @@ String confDir = "${appDir}conf/"
 String i18Dir = "${appDir}i18n/"
 
 // copy readme into project
-moveFile(source, dest, "${appDir}README", "README-Pref")
+copyFile(source, dest, "${appDir}README", "README-Pref")
 print('------------')
 print('Algospref - creato (o sovrascritto) README-Pref')
 print('------------')
 
-// copy LogoBootStrap into project
+// copy PrefBootStrap into project
 moveFile(source, dest, "${confDir}PrefBootStrap.groovy")
 print('------------')
 print('Algospref - creato (NON sovrascritto) PrefBootStrap')
 print('------------')
 
 // copy i18n into project
-moveFile(source, dest, "${i18Dir}algospref.properties")
+copyFile(source, dest, "${i18Dir}algospref.properties")
 print('------------')
 print('Algospref - creato (o sovrascritto) grails-app/i18n/algospref.properties')
 print('------------')
 
+public static copyFile(String srcDirPath, String dstDirPath, String fileName) {
+    copyFile(srcDirPath, dstDirPath, fileName, fileName)
+} // fine del metodo
 
 public static moveFile(String srcDirPath, String dstDirPath, String fileName) {
     moveFile(srcDirPath, dstDirPath, fileName, fileName)
+} // fine del metodo
+
+public static copyFile(String srcDirPath, String dstDirPath, String srcFileName, String dstFileName) {
+    String srcFile = srcDirPath + srcFileName
+    String destFile = dstDirPath + dstFileName
+
+    copyFile(srcFile, destFile)
+    deleteFile(srcFile)
 } // fine del metodo
 
 public static moveFile(String srcDirPath, String dstDirPath, String srcFileName, String dstFileName) {
@@ -45,4 +56,8 @@ public static moveFile(String srcDirPath, String dstDirPath, String srcFileName,
 
 public static copyFile(String srcFile, String destFile) {
     new AntBuilder().copy(file: srcFile, tofile: destFile, overwrite: true)
+} // fine del metodo
+
+public static deleteFile(String pathFile) {
+    new AntBuilder().delete(file: pathFile)
 } // fine del metodo
