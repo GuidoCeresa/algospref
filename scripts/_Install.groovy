a4e02ccfb572575a6ee3c663ee96dd7cb305c38a
GuidoCeresa <gac@algos.it>
2014-06-30 10:33:58 +0200

    0.71 install

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -2,25 +2,47 @@
  * Main script to setup algospref on installation
  */
 
-def sourceFile
-def targetFile
+//--utilizza le special variables provided by Gant
+String source = "${pluginBasedir}"
+String dest = "${basedir}"
+source = dest + "/" + source + "/"
+dest = dest + "/"
+
+//--directory dell'applicazione
+String appDir = "grails-app/"
+String confDir = "${appDir}conf/"
+String i18Dir = "${appDir}i18n/"
+
+// copy readme into project
+moveFile(source, dest, "${appDir}README", "README-Pref")
+print('------------')
+print('Algospref - creato (o sovrascritto) README-Pref')
+print('------------')
 
 // copy LogoBootStrap into project
-sourceFile = "${pluginBasedir}/grails-app/conf/PrefBootStrap.groovy"
-targetFile = "${basedir}/grails-app/conf/PrefBootStrap.groovy"
-ant.copy(file: sourceFile, tofile: targetFile, overwrite: false)
-ant.delete(file: sourceFile)
-
+moveFile(source, dest, "${confDir}PrefBootStrap.groovy")
 print('------------')
 print('Algospref - creato (NON sovrascritto) PrefBootStrap')
 print('------------')
 
 // copy i18n into project
-sourceFile = "${pluginBasedir}/grails-app/i18n/algospref.properties"
-targetFile = "${basedir}/grails-app/i18n/algospref.properties"
-ant.copy(file: sourceFile, tofile: targetFile, overwrite: true)
-
+moveFile(source, dest, "${i18Dir}algospref.properties")
 print('------------')
 print('Algospref - creato (o sovrascritto) grails-app/i18n/algospref.properties')
 print('------------')
 
+
+public static moveFile(String srcDirPath, String dstDirPath, String fileName) {
+    moveFile(srcDirPath, dstDirPath, fileName, fileName)
+} // fine del metodo
+
+public static moveFile(String srcDirPath, String dstDirPath, String srcFileName, String dstFileName) {
+    String srcFile = srcDirPath + srcFileName
+    String destFile = dstDirPath + dstFileName
+
+    copyFile(srcFile, destFile)
+} // fine del metodo
+
+public static copyFile(String srcFile, String destFile) {
+    new AntBuilder().copy(file: srcFile, tofile: destFile, overwrite: true)
+} // fine del metodo
